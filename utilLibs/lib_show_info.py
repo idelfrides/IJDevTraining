92c6a9f908ebf418618130a3da24e8ae4df2087a
Idelfrides Jorge <idelfrides.papai@relyance.ai>
2022-10-23 21:52:35 -0300

    reorganizing the libs folder

---
+++ utilLibs/lib_show_info.py
@@ -0,0 +1,18 @@
+
+"""#### Show informations module"""
+
+
+def end_app():
+    """# Finish app execution
+
+    Returns:
+        None: None
+    """
+
+    print('\n\n\n')
+    print('#'*80)
+    print('\n\t\t\t\t END RUNNING APP \n')
+    print('#'*80)
+    print('\n\n')
+
+    return
